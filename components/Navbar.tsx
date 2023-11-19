1e61f55a1f594a92214c6c7082a7352e97523a18
Darlan Costa <darlandsv@gmail.com>
2023-11-19 13:24:48 -0300

    fix icon

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 'use client'; // this is a client component
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { Link } from 'react-scroll/modules';
 import { usePathname } from 'next/navigation';
@@ -32,11 +32,19 @@ const NAV_ITEMS: Array<NavItem> = [
 ];
 
 export default function Navbar() {
+  const [mounted, setMounted] = useState(false);
   const { systemTheme, theme, setTheme } = useTheme();
   const currentTheme = theme === 'system' ? systemTheme : theme;
-
-  const pathname = usePathname();
   const [navbar, setNavbar] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
   return (
     <header className="w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600">
       <div className="justify-between md:items-center md:flex">
